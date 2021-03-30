43faeac8338d6fcb4008e31f3523d6131edf2499
Finn Hermansson <finn.hermansson@svt.se>
2021-03-30 08:56:55 +0200

    More fraction improvements
    Signed-off-by: Finn Hermansson <finn.hermansson@svt.se>

--- src/main/kotlin/se/svt/oss/mediaanalyzer/file/Fraction.kt
+++ src/main/kotlin/se/svt/oss/mediaanalyzer/file/Fraction.kt
@@ -4,40 +4,100 @@
 
 package se.svt.oss.mediaanalyzer.file
 
-data class Fraction(val numerator: Int, val denominator: Int) {
+class Fraction(num: Int, den: Int) : Comparable<Fraction> {
     companion object {
         private val regex = Regex("(\\d+)[/:](\\d+)")
 
         @JvmStatic
-        fun parse(value: String): Fraction {
+        fun parse(value: String) = parseOrNull(value)
+            ?: throw IllegalArgumentException("$value is not a valid fraction!")
+
+        @JvmStatic
+        fun parseOrNull(value: String): Fraction? {
             val matchResult =
-                regex.matchEntire(value) ?: throw IllegalArgumentException("$value is not a valid fraction!")
-            val nominator = matchResult.groupValues[1].toInt()
+                regex.matchEntire(value) ?: return null
+            val numerator = matchResult.groupValues[1].toInt()
             val denominator = matchResult.groupValues[2].toInt()
-            return Fraction(nominator, denominator)
+            if (denominator == 0) return null
+            return Fraction(numerator, denominator)
         }
 
-        private fun gcd(a: Int, b: Int): Int {
+        @JvmStatic
+        fun gcd(a: Int, b: Int): Int {
             return if (b == 0) a else gcd(b, a % b)
         }
     }
 
-    fun simplified(): Fraction {
-        val gcd = gcd(numerator, denominator)
-        return Fraction(numerator / gcd, denominator / gcd)
+    val numerator: Int
+    val denominator: Int
+
+    init {
+        if (den == 0) throw IllegalArgumentException("denominator must not be 0!")
+        val gcd = gcd(num, den)
+        numerator = num / gcd
+        denominator = den / gcd
     }
+
+    operator fun plus(other: Fraction): Fraction {
+        val num = (numerator * other.denominator) + (other.numerator * denominator)
+        val den = denominator * other.denominator
+        return Fraction(num, den)
+    }
+
+    operator fun minus(other: Fraction): Fraction {
+        val num = (numerator * other.denominator) - (other.numerator * denominator)
+        val den = denominator * other.denominator
+        return Fraction(num, den)
+    }
+
+    operator fun times(other: Fraction) =
+        Fraction(
+            numerator * other.numerator,
+            denominator * other.denominator
+        )
+
+    operator fun div(other: Fraction) =
+        Fraction(
+            numerator * other.denominator,
+            denominator * other.numerator
+        )
 
     @JvmOverloads
     fun stringValue(delimiter: String = "/") = "$numerator$delimiter$denominator"
 
-    val isValid: Boolean
-        get() = denominator != 0
-
     fun toDouble(): Double = numerator.toDouble() / denominator
 
-    fun toDoubleSafe() = if (isValid) toDouble() else null
+    override fun toString() = "$numerator/$denominator"
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Fraction
+
+        if (numerator != other.numerator) return false
+        if (denominator != other.denominator) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = numerator
+        result = 31 * result + denominator
+        return result
+    }
+
+    override fun compareTo(other: Fraction): Int {
+        if (this == other) {
+            return 0
+        }
+        return (numerator * other.denominator)
+            .compareTo(denominator * other.numerator)
+    }
 }
 
 typealias FractionString = String
 
 fun FractionString.toFraction() = Fraction.parse(this)
+fun FractionString.toFractionOrNull() = Fraction.parseOrNull(this)
+fun Int.toFraction() = Fraction(this, 1)
--- src/test/kotlin/se/svt/oss/mediaanalyzer/file/FractionTest.kt
+++ src/test/kotlin/se/svt/oss/mediaanalyzer/file/FractionTest.kt
@@ -12,17 +12,26 @@ internal class FractionTest {
 
     @Test
     fun testParse() {
-        val fraction = "22/11".toFraction()
+        val fraction = "720/576".toFraction()
         assertThat(fraction)
-            .hasNumerator(22)
-            .hasDenominator(11)
-        assertThat("22:11".toFraction()).isEqualTo(fraction)
+            .hasNumerator(5)
+            .hasDenominator(4)
+        assertThat("720:576".toFraction()).isEqualTo(fraction)
+        assertThat("720:576".toFractionOrNull()).isEqualTo(fraction)
     }
 
     @Test
-    fun testParseInvalid() {
-        assertThatThrownBy { Fraction.parse("apa") }
-            .hasMessage("apa is not a valid fraction!")
+    fun testInvalid() {
+        listOf("10/0", "xxx", "10:p").forEach {
+            assertThatThrownBy { Fraction.parse(it) }
+                .hasMessage("$it is not a valid fraction!")
+            assertThatThrownBy { it.toFraction() }
+                .hasMessage("$it is not a valid fraction!")
+            assertThat(Fraction.parseOrNull(it)).isNull()
+            assertThat(it.toFractionOrNull()).isNull()
+        }
+        assertThatThrownBy { Fraction(1, 0) }
+            .hasMessage("denominator must not be 0!")
     }
 
     @Test
@@ -31,23 +40,49 @@ internal class FractionTest {
     }
 
     @Test
-    fun testInvalid() {
-        val fraction = Fraction(0, 0)
-        assertThat(fraction).isNotValid
-        assertThat(fraction.toDoubleSafe()).isNull()
+    fun testIntToFraction() {
+        assertThat(5.toFraction()).isEqualTo(Fraction(5, 1))
+    }
+
+    @Test
+    fun testHashCode() {
+        assertThat(Fraction(7, 8)).hasSameHashCodeAs(31 * 7 + 8)
     }
 
     @Test
     fun testSimplified() {
-        assertThat(Fraction(0, 4).simplified()).isEqualTo(Fraction(0, 1))
-        assertThat(Fraction(4, 1).simplified()).isEqualTo(Fraction(4, 1))
-        assertThat(Fraction(1080, 1920).simplified()).isEqualTo(Fraction(9, 16))
+        assertThat(Fraction(0, 4)).isEqualTo(Fraction(0, 1))
+        assertThat(Fraction(4, 1)).isEqualTo(Fraction(4, 1))
+        assertThat(Fraction(1080, 1920)).isEqualTo(Fraction(9, 16))
     }
 
     @Test
     fun testStringValue() {
         assertThat(Fraction(16, 9).stringValue()).isEqualTo("16/9")
+        assertThat(Fraction(16, 9)).hasToString("16/9")
         assertThat(Fraction(16, 9).stringValue(":")).isEqualTo("16:9")
         assertThat(Fraction(16, 9).stringValue("/")).isEqualTo("16/9")
+    }
+
+    @Test
+    fun testCompareTo() {
+        listOf(
+            Pair("16:9", "4:3"),
+            Pair("50/1", "25/1"),
+            Pair("4:3", "1:1")
+        ).map { Pair(it.first.toFraction(), it.second.toFraction()) }
+            .forEach {
+                assertThat(it.first > it.second).`as`("${it.first} > ${it.second}").isTrue()
+                assertThat(it.second < it.first).`as`("${it.second} < ${it.first}").isTrue()
+            }
+        assertThat(Fraction(16, 9).compareTo(Fraction(1920, 1080))).isEqualTo(0)
+    }
+
+    @Test
+    fun testArithmetic() {
+        assertThat(Fraction(7, 4) + Fraction(2, 7)).`as`("7/4 + 2/7").isEqualTo(Fraction(57, 28))
+        assertThat(Fraction(7, 4) - Fraction(2, 7)).`as`("7/4 - 2/7").isEqualTo(Fraction(41, 28))
+        assertThat(Fraction(7, 4) / Fraction(2, 7)).`as`("7/4 / 2/7").isEqualTo(Fraction(49, 8))
+        assertThat(Fraction(7, 4) * Fraction(2, 7)).`as`("7/4 * 2/7").isEqualTo(Fraction(1, 2))
     }
 }
