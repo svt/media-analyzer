6e912d8fe35ce204cb96c37c604934b022be7fef
Finn Hermansson <finn.hermansson@svt.se>
2021-03-31 13:45:08 +0200

    Fraction improvements (#2)
    * simplify fractions
    
    Signed-off-by: Finn Hermansson <finn.hermansson@svt.se>
    
    * More fraction improvements
    
    Signed-off-by: Finn Hermansson <finn.hermansson@svt.se>
    
    * Use commons-math Fraction
    
    Signed-off-by: Finn Hermansson <finn.hermansson@svt.se>

--- src/test/kotlin/se/svt/oss/mediaanalyzer/file/FractionTest.kt
+++ src/test/kotlin/se/svt/oss/mediaanalyzer/file/FractionTest.kt
@@ -4,6 +4,7 @@
 
 package se.svt.oss.mediaanalyzer.file
 
+import org.apache.commons.math3.fraction.Fraction
 import org.junit.jupiter.api.Test
 import se.svt.oss.mediaanalyzer.Assertions.assertThat
 import se.svt.oss.mediaanalyzer.Assertions.assertThatThrownBy
@@ -12,29 +13,43 @@ internal class FractionTest {
 
     @Test
     fun testParse() {
-        val fraction = "22/11".toFraction()
+        val fraction = "720/576".toFraction()
         assertThat(fraction)
-            .hasNumerator(22)
-            .hasDenominator(11)
-            .hasStringValue("22/11")
-        assertThat("22:11".toFraction()).isEqualTo(fraction)
+            .hasNumerator(5)
+            .hasDenominator(4)
+        assertThat("720:576".toFraction()).isEqualTo(fraction)
+        assertThat("720:576".toFractionOrNull()).isEqualTo(fraction)
+        assertThat("3".toFraction()).isEqualTo(Fraction(3))
     }
 
     @Test
     fun testParseInvalid() {
-        assertThatThrownBy { Fraction.parse("apa") }
-            .hasMessage("apa is not a valid fraction!")
+        listOf("10/0", "xxx", "10:p").forEach {
+            assertThatThrownBy { parse(it) }
+                .hasMessage("$it is not a valid fraction!")
+            assertThatThrownBy { it.toFraction() }
+                .hasMessage("$it is not a valid fraction!")
+            assertThat(parseOrNull(it)).isNull()
+            assertThat(it.toFractionOrNull()).isNull()
+        }
     }
 
     @Test
-    fun testToDouble() {
-        assertThat(Fraction(1, 2).toDouble()).isEqualTo(0.5)
+    fun testStringValue() {
+        assertThat("4:3".toFraction().stringValue()).isEqualTo("4/3")
+        assertThat("4:3".toFraction().stringValue(":")).isEqualTo("4:3")
     }
 
     @Test
-    fun testInvalid() {
-        val fraction = Fraction(0, 0)
-        assertThat(fraction).isNotValid
-        assertThat(fraction.toDoubleSafe()).isNull()
+    fun testIntToFraction() {
+        assertThat(5.toFraction()).isEqualTo(Fraction(5, 1))
+    }
+
+    @Test
+    fun testOperatorFunctions() {
+        assertThat(Fraction(7, 4) + Fraction(2, 7)).`as`("7/4 + 2/7").isEqualTo(Fraction(57, 28))
+        assertThat(Fraction(7, 4) - Fraction(2, 7)).`as`("7/4 - 2/7").isEqualTo(Fraction(41, 28))
+        assertThat(Fraction(7, 4) / Fraction(2, 7)).`as`("7/4 / 2/7").isEqualTo(Fraction(49, 8))
+        assertThat(Fraction(7, 4) * Fraction(2, 7)).`as`("7/4 * 2/7").isEqualTo(Fraction(1, 2))
     }
 }
