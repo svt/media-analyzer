e7b6e89c94aad8c6a08d8b7132d51607d142adb0
Josef Andersson <josef.andersson@svt.se>
2021-08-11 14:42:33 +0200

    fix!: use java 11. bump a few deps
    Signed-off-by: Josef Andersson <josef.andersson@svt.se>

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,11 @@ plugins {
     id("maven-publish")
     idea
     jacoco
-    kotlin("jvm") version "1.4.20"
+    kotlin("jvm") version "1.4.31"
     id("com.github.fhermansson.assertj-generator") version "1.1.2"
-    id("org.jmailen.kotlinter") version "3.1.0"
-    id("pl.allegro.tech.build.axion-release") version "1.10.2"
-    id("se.ascp.gradle.gradle-versions-filter") version "0.1.8"
+    id("org.jmailen.kotlinter") version "3.4.5"
+    id("pl.allegro.tech.build.axion-release") version "1.13.3"
+    id("se.ascp.gradle.gradle-versions-filter") version "0.1.10"
     id("se.svt.oss.gradle-yapp-publisher-plugin") version "0.1.13"
 }
 
@@ -42,28 +42,26 @@ repositories {
 
 
 dependencies {
-    implementation(kotlin("stdlib-jdk8"))
-    implementation(kotlin("reflect"))
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.10.2")
-    implementation("io.github.microutils:kotlin-logging:1.7.9")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.4")
+    implementation("io.github.microutils:kotlin-logging:2.0.10")
     api("org.apache.commons:commons-math3:3.6.1")
     testImplementation("io.mockk:mockk:1.10.0")
     testImplementation("org.assertj:assertj-core:3.16.1")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
-    testRuntimeOnly("ch.qos.logback:logback-classic:1.2.3")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.2")
+    testRuntimeOnly("ch.qos.logback:logback-classic:1.2.5")
 }
 
 tasks {
     compileKotlin {
-        kotlinOptions.jvmTarget = "1.8"
+        kotlinOptions.jvmTarget = "11"
     }
     compileTestKotlin {
-        kotlinOptions.jvmTarget = "1.8"
+        kotlinOptions.jvmTarget = "11"
     }
 }
 
 tasks.wrapper {
     distributionType = Wrapper.DistributionType.ALL
-    gradleVersion = "6.8.3"
+    gradleVersion = "6.9"
 }
