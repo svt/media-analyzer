b53ce28bb08fb0dc56800f774d4c9ed1f446fdd3
Josef Andersson <37870813+janderssonse@users.noreply.github.com>
2021-08-11 16:19:24 +0200

    fix!: use yapp publisher plugin. correct gh actions. java 11 (#3)
    * use yapp publisher plugin. correct gh actions
    
    Signed-off-by: Josef Andersson <josef.andersson@svt.se>
    
    * bump yapp,versions plugin
    
    Signed-off-by: Josef Andersson <josef.andersson@svt.se>
    
    * add missing version
    
    Signed-off-by: Josef Andersson <josef.andersson@svt.se>
    
    * fix!: use java 11. bump a few deps
    
    Signed-off-by: Josef Andersson <josef.andersson@svt.se>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,22 @@
 plugins {
-    `maven-publish`
-    signing
+    id("maven-publish")
     idea
     jacoco
-    kotlin("jvm") version "1.3.72"
+    kotlin("jvm") version "1.4.31"
     id("com.github.fhermansson.assertj-generator") version "1.1.2"
-    id("org.jmailen.kotlinter") version "2.3.2"
-    id("pl.allegro.tech.build.axion-release") version "1.10.2"
-    id("com.github.ben-manes.versions") version "0.28.0"
+    id("org.jmailen.kotlinter") version "3.4.5"
+    id("pl.allegro.tech.build.axion-release") version "1.13.3"
+    id("se.ascp.gradle.gradle-versions-filter") version "0.1.10"
+    id("se.svt.oss.gradle-yapp-publisher-plugin") version "0.1.13"
 }
 
-val PUBLISH_GROUP_ID by extra("se.svt.oss")
-val PUBLISH_VERSION by extra(scmVersion.version)
-val PUBLISH_ARTIFACT_ID by extra("media-analyzer")
 
-group = PUBLISH_GROUP_ID
-project.version = PUBLISH_VERSION
+group = "se.svt.oss"
+project.version = scmVersion.version
 
 apply {
     from("initial-version.gradle")
     from("checks.gradle")
-    from("publishing.gradle")
 }
 
 tasks.test {
@@ -46,27 +42,26 @@ repositories {
 
 
 dependencies {
-    implementation(kotlin("stdlib-jdk8"))
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.10.2")
-    implementation("io.github.microutils:kotlin-logging:1.7.9")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.4")
+    implementation("io.github.microutils:kotlin-logging:2.0.10")
     api("org.apache.commons:commons-math3:3.6.1")
     testImplementation("io.mockk:mockk:1.10.0")
     testImplementation("org.assertj:assertj-core:3.16.1")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
-    testRuntimeOnly("ch.qos.logback:logback-classic:1.2.3")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.2")
+    testRuntimeOnly("ch.qos.logback:logback-classic:1.2.5")
 }
 
 tasks {
     compileKotlin {
-        kotlinOptions.jvmTarget = "1.8"
+        kotlinOptions.jvmTarget = "11"
     }
     compileTestKotlin {
-        kotlinOptions.jvmTarget = "1.8"
+        kotlinOptions.jvmTarget = "11"
     }
 }
 
 tasks.wrapper {
     distributionType = Wrapper.DistributionType.ALL
-    gradleVersion = "6.4.1"
+    gradleVersion = "6.9"
 }
