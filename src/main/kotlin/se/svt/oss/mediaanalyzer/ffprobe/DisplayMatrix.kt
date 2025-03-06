aafce20640c4f7a908a748134f69bcdc1d38b633
Finn Hermansson <finn.hermansson@svt.se>
2025-03-06 14:06:53 +0100

    Add licence info to new files. Remove mega-linter
    Signed-off-by: Finn Hermansson <finn.hermansson@svt.se>

--- src/main/kotlin/se/svt/oss/mediaanalyzer/ffprobe/DisplayMatrix.kt
+++ src/main/kotlin/se/svt/oss/mediaanalyzer/ffprobe/DisplayMatrix.kt
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2020 Sveriges Television AB
+//
+// SPDX-License-Identifier: Apache-2.0
+
 package se.svt.oss.mediaanalyzer.ffprobe
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
